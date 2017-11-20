3b0a5d762327d84c40a80bcca252ed5c72f7349b
damien toulouse <dam.toulouse@gmail.com>
2017-11-20 11:41:12 +0100

    std spring skeleton

---
+++ src/main/java/toulouse/aoudia/legendary_crafter/LegendaryCrafterApplication.java
@@ -0,0 +1,12 @@
+package toulouse.aoudia.legendary_crafter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegendaryCrafterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegendaryCrafterApplication.class, args);
+	}
+}
---
+++ src/test/java/toulouse/aoudia/legendary_crafter/LegendaryCrafterApplicationTests.java
@@ -0,0 +1,16 @@
+package toulouse.aoudia.legendary_crafter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LegendaryCrafterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
