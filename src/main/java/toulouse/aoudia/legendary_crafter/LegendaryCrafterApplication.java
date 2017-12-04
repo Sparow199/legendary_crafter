72c4e3be0daf009a8b549e9ffa5b890c9c7c85e9
damien toulouse <dam.toulouse@gmail.com>
2017-12-04 09:24:59 +0100

    add database driver connection

--- src/main/java/toulouse/aoudia/legendary_crafter/LegendaryCrafterApplication.java
+++ src/main/java/toulouse/aoudia/legendary_crafter/LegendaryCrafterApplication.java
@@ -1,5 +1,8 @@
 package toulouse.aoudia.legendary_crafter;
 
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoDatabase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,5 +11,8 @@ public class LegendaryCrafterApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LegendaryCrafterApplication.class, args);
+		MongoClientURI uri = new MongoClientURI("mongodb://legendary_admin:<PASSWORD>@legendarycrafterdb-shard-00-00-dmwgc.mongodb.net:27017,legendarycrafterdb-shard-00-01-dmwgc.mongodb.net:27017,legendarycrafterdb-shard-00-02-dmwgc.mongodb.net:27017/test?ssl=true&replicaSet=legendaryCrafterDb-shard-0&authSource=admin");
+		MongoClient mongoClient = new MongoClient(uri);
+		MongoDatabase database = mongoClient.getDatabase("test");
 	}
 }
