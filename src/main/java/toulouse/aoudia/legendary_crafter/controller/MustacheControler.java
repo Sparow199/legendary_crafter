3306552bd48341b755c296a1bfe7488262e4dd79
Aoudia <mf.aoudia@gmail.com>
2017-12-18 18:20:47 +0100

    Add create page

--- src/main/java/toulouse/aoudia/legendary_crafter/controller/MustacheControler.java
+++ src/main/java/toulouse/aoudia/legendary_crafter/controller/MustacheControler.java
@@ -9,7 +9,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import toulouse.aoudia.legendary_crafter.model.User;
 import toulouse.aoudia.legendary_crafter.service.HeroService;
+import toulouse.aoudia.legendary_crafter.service.UserService;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -20,6 +22,8 @@ import java.util.Map;
 public class MustacheControler {
     @Autowired
     HeroService heroService;
+    @Autowired
+    UserService userService;
 
     @RequestMapping("/")
     public String indexPage(){
@@ -30,6 +34,19 @@ public class MustacheControler {
     public String loginPage(){
         return "login";
     }
+
+    @RequestMapping(value = "/user/create",method = RequestMethod.GET)
+    public String userCreatPage(){
+        return "create";
+    }
+
+    @RequestMapping(value = "/user/create",method = RequestMethod.POST)
+    public String userCreatPost(@RequestParam String username, @RequestParam String password){
+        User user = new User(username,password);
+        userService.saveUser(user);
+        return "create";
+    }
+
 
     @RequestMapping(value = "/auth",method = RequestMethod.POST)
     public void login(@RequestParam Map<String, String> map, HttpServletRequest request, HttpServletResponse response){
